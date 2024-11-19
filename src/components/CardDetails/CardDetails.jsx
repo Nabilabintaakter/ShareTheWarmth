85d2159537de08619a296dbf206611ccb0abb54b
Nabilabintaakter <nabilabintaakter@gmail.com>
2024-11-20 02:21:25 +0600

    added a login form in the login page

--- src/components/CardDetails/CardDetails.jsx
+++ src/components/CardDetails/CardDetails.jsx
@@ -17,15 +17,15 @@ const CardDetails = () => {
 
     return (
         <div className="w-[75%] max-w-7xl mx-auto pt-5 pb-12 md:pt-10 md:pb-24">
-            <div className="card lg:card-side bg-base-100 shadow-lg">
-                <figure>
+            <div className="flex flex-col lg:flex-row rounded-2xl bg-base-100 shadow-lg">
+                <figure className='w-full lg:w-1/2'>
                     <img
-                        className="w-full h-full object-cover"
+                        className="w-full h-full object-cover rounded-t-2xl lg:rounded-l-2xl lg:rounded-r-none"
                         src={image}
                         alt="Album"
                     />
                 </figure>
-                <div className="p-5 lg:px-6 flex flex-col gap-3 lg:justify-between">
+                <div className="w-full lg:w-1/2 p-5 lg:px-6 flex flex-col gap-3 lg:justify-between">
                     <h2 className="card-title text-2xl font-semibold">{title}</h2>
                     <p className="text-gray-500">{description}</p>
                     <p className={`w-fit badge badge-md border-[1px] ${status === 'Ongoing' ? 'bg-green-400 border-green-700' : 'bg-gray-400 border-gray-600'}`}>{status}</p>
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,80 @@
-import React from 'react';
+import bg from '../../assets/how-bg-3.jpg';
+import { FcGoogle } from "react-icons/fc";
+import { NavLink } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            Login
+        <div 
+            className="py-16 lg:py-24 relative flex flex-col items-center min-h-screen" 
+            style={{
+                backgroundImage: `url(${bg})`,
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+            }}
+        >
+            <div className="text-center mb-8">
+                <h1 className="text-4xl lg:text-5xl font-extrabold mb-2">Welcome Back!</h1>
+                <p className="text-2xl lg:text-3xl font-semibold text-gray-700">Please Log In</p>
+            </div>
+            <div className="bg-white/30 backdrop-blur-lg shadow-2xl rounded-lg w-[90%] md:w-[60%] lg:w-[40%] px-6 py-8 max-w-xl">
+                <form className="space-y-6">
+                    <div className="form-control">
+                        <label className="block text-lg font-semibold text-gray-800 mb-1">Email :</label>
+                        <input 
+                            type="email" 
+                            name="email" 
+                            placeholder="Enter your email" 
+                            className="input input-bordered w-full py-3 px-4 rounded-md border border-gray-300 focus:ring-2 focus:ring-blue-400 focus:outline-none" 
+                            required 
+                        />
+                    </div>
+                    <div className="form-control">
+                        <label className="block text-lg font-semibold text-gray-800 mb-1">Password :</label>
+                        <input 
+                            type="password" 
+                            name="password" 
+                            placeholder="Enter your password" 
+                            className="input input-bordered w-full py-3 px-4 rounded-md border border-gray-300 focus:ring-2 focus:ring-blue-400 focus:outline-none" 
+                            required 
+                        />
+                    </div>
+                    <div className="text-right">
+                        <NavLink 
+                            to="#" 
+                            className="text-sm text-blue-600 hover:underline">
+                            Forgot password?
+                        </NavLink>
+                    </div>
+                    <div className="form-control mt-4">
+                        <button 
+                            type="submit" 
+                            className="w-full bg-blue-600 text-white py-3 rounded-md text-lg border-[1px] border-blue-600 font-medium hover:bg-white hover:text-blue-600 hover:border-[1px] hover:border-blue-600 duration-300 transition-all hover:font-bold"
+                        >
+                            Login
+                        </button>
+                    </div>
+                </form>
+                <div className="my-6 border-t border-gray-300"></div>
+                <div className="form-control">
+                    <NavLink 
+                        to="#"
+                        className="flex items-center justify-center gap-3 bg-red-500 text-white py-3 rounded-md text-lg font-medium border-[1px] border-red-500 hover:bg-white hover:border hover:border-red-500 hover:text-red-500 hover:font-bold transition-all duration-300"
+                    >
+                        <FcGoogle className="text-2xl" />
+                        Login with Google
+                    </NavLink>
+                </div>
+                <p className="text-center mt-6 text-gray-700">
+                    Don't have an account?  
+                    <NavLink 
+                        to="/register" 
+                        className="text-blue-600 ml-1 font-semibold hover:underline"
+                    >
+                        Register
+                    </NavLink>
+                </p>
+            </div>
         </div>
     );
 };
