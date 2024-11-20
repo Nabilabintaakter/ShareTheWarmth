3a96174a2a4c8fac0c8ca3ba2b436229ec3ccbed
Nabilabintaakter <nabilabintaakter@gmail.com>
2024-11-21 04:10:10 +0600

    implemented smooth scroll-to-top functionality on route change for better user experience

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -2,11 +2,13 @@
 import Footer from '../Footer/Footer';
 import Navbar from '../Navbar/Navbar';
 import { Outlet ,useLocation} from 'react-router-dom';
+import ScrollToTop from '../ScrollToTop/ScrollToTop';
 
 const MainLayout = () => {
     const {pathname} = useLocation();
     return (
         <div className='font-nunito'>
+            <ScrollToTop></ScrollToTop>
             <div className={`${pathname === '/' ? 'h-0': 'h-16'}`}>
                 <Navbar></Navbar>
             </div>
